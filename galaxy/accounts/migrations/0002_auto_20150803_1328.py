0738d55d611f0b3e0a6615a58417e2fcc22fe150
Chris Houseknecht <chouseknecht@ansible.com>
2015-08-03 13:46:50 -0400

    Custom Migration
    When building a database from a  production backup seeing a not null constraint on accounts_customuser.last_login. It seems the base account object changed in the more recent django_allauth version. The current model allows last_login to be null.

---
+++ galaxy/accounts/migrations/0002_auto_20150803_1328.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL("ALTER TABLE accounts_customuser ALTER COLUMN last_login DROP NOT NULL;")
+    ]
