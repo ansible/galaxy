d121d14497fcdb264b265c75d93729c2d1f74637
Chris Houseknecht <chouseknecht@ansible.com>
2015-08-05 16:45:09 -0400

    Issue #2 - Fix "no ___ found" visual flicker
    Applied the spinner to Explore page. Seems to be working. Fix bad sort orders. The API is sending numeric data back as string.

--- galaxy/static/js/controllers/main.js
+++ galaxy/static/js/controllers/main.js
@@ -19,62 +19,83 @@
 
 'use strict';
 
-/* Controllers */
+(function(angular) {
 
-var mainControllers = angular.module('mainControllers', []);
+    angular.module('mainControllers', [])
+        .controller('MainCtrl', ['$scope', '$timeout', 'roleFactory', 'userFactory', 'categoryFactory', _controller]);
 
-mainControllers.controller('MainCtrl', ['$scope', 'roleFactory', 'userFactory', 'categoryFactory',
-function($scope, roleFactory, userFactory, categoryFactory) {
+    function _controller($scope, $timeout, roleFactory, userFactory, categoryFactory) {
 
-    $scope.page_title = 'Explore';
-    $scope.results_per_page = 10;
-    $scope.loading = 0;
+        $scope.page_title = 'Explore';
+        $scope.results_per_page = 10;
 
-    $scope.data_names = ["categories","top_roles","new_roles","top_users","top_reviewers","new_users"];
+        $scope.loading = {
+            categories: 1,
+            topRoles: 1,
+            newRoles: 1,
+            topUsers: 1,
+            topReviewers: 1,
+            newUsers: 1
+        };
 
-    $scope.restore_state = function () {
-        $scope.data_names.forEach(function (entry) {
-            console.log("entry is "+entry);
-            var default_sort_col = '';
-            var more_link = '';
-            var data_function = null;
-            if (entry == 'categories') {
-                default_sort_col = 'num_roles';
-                more_link = '/list#/roles/';
-                data_function = $scope.getCategories;
+        $scope.toggle_item = function (item, sort_col) {
+            if (item.sort_col != sort_col) {
+                item.sort_col = sort_col;
+                item.data_function();
             }
-            else if (entry == 'top_roles') {
-                default_sort_col = 'average_score';
-                more_link = '/list#/roles/sort/sort-by-community-score';
-                data_function = $scope.getTopRoles;
-            }
-            else if (entry == 'new_roles') {
-                default_sort_col = 'created';
-                more_link = '/list#/roles/sort/sort-by-created-on-date';
-                data_function = $scope.getNewRoles;
-            }
-            else if (entry == 'top_users') {
-                default_sort_col = 'avg_role_score';
-                more_link = '/list#/users/sort/sort-by-community-score';
-                data_function = $scope.getTopUsers;
-            }
-            else if (entry == 'top_reviewers') {
-                default_sort_col = 'num_ratings';
-                more_link = '/list#/users/sort/sort-by-top-reviewers';
-                data_function = $scope.getTopReviewers;
-            }
-            else if (entry == 'new_users') {
-                default_sort_col = 'date_joined';
-                more_link = '/list#/users/sort/sort-by-joined-on-date';
-                data_function = $scope.getNewUsers;
-            }
-            /*
-            if (typeof(Storage) !== "undefined" && localStorage[entry]) {
-                //if local storage is available, and we previously stored a value
-                $scope[entry] = JSON.parse(localStorage[entry]);
-            }
-            else {
-            */
+        };
+
+        $timeout(function() {
+            // give the partial templates a chance to load before we do this...
+            _restoreState();
+            _getCategories();
+            _getTopRoles();
+            _getNewRoles();
+            _getTopUsers();
+            _getTopReviewers();
+            _getNewUsers();
+        }, 300);
+
+        return;
+
+
+        function _restoreState() {
+            var data_names = ["categories","top_roles","new_roles","top_users","top_reviewers","new_users"];
+            data_names.forEach(function (entry) {
+                var default_sort_col = '';
+                var more_link = '';
+                var data_function = null;
+                if (entry === 'categories') {
+                    default_sort_col = 'num_roles';
+                    more_link = '/list#/roles/';
+                    data_function = _getCategories;
+                }
+                else if (entry === 'top_roles') {
+                    default_sort_col = 'average_score';
+                    more_link = '/list#/roles/sort/sort-by-community-score';
+                    data_function = _getTopRoles;
+                }
+                else if (entry === 'new_roles') {
+                    default_sort_col = 'created';
+                    more_link = '/list#/roles/sort/sort-by-created-on-date';
+                    data_function = _getNewRoles;
+                }
+                else if (entry === 'top_users') {
+                    default_sort_col = 'avg_role_score';
+                    more_link = '/list#/users/sort/sort-by-community-score';
+                    data_function = _getTopUsers;
+                }
+                else if (entry === 'top_reviewers') {
+                    default_sort_col = 'num_ratings';
+                    more_link = '/list#/users/sort/sort-by-top-reviewers';
+                    data_function = _getTopReviewers;
+                }
+                else if (entry === 'new_users') {
+                    default_sort_col = 'date_joined';
+                    more_link = '/list#/users/sort/sort-by-joined-on-date';
+                    data_function = _getNewUsers;
+                }
+
                 $scope[entry] = {
                     'page': 1,
                     'data': [],
@@ -82,121 +103,119 @@ function($scope, roleFactory, userFactory, categoryFactory) {
                     'reverse': true,
                     'more_link' : more_link,
                     'data_function': data_function
-                    }
-            //}
+                };
             });
-        };
+        }
 
-    if ($scope.removeSaveState) {
-        $scope.removeSaveState();
+        function _getCategories() {
+            categoryFactory.getCategories(
+                $scope.categories.sort_col,
+                $scope.categories.reverse
+                )
+                .success(function (data) {
+                    $scope.categories.data = data.slice(0, $scope.results_per_page);
+                    $scope.loading.categories = 0;
+                })
+                .error(function (error) {
+                    $scope.loading.categories = 0;
+                    $scope.categories.data = [];
+                });
+        }
+
+        function _getTopRoles() {
+            roleFactory.getLatest(
+                $scope.top_roles.page,
+                $scope.results_per_page,
+                $scope.top_roles.sort_col,
+                $scope.top_roles.reverse
+                )
+                .success(function (data) {
+                    data['results'].forEach(function(row) {
+                        row.average_score = parseFloat(row.average_score);
+                    });
+                    $scope.top_roles.data = data['results'];
+                    $scope.loading.topRoles = 0;
+                })
+                .error(function (error) {
+                    $scope.loading.topRoles = 0;
+                    $scope.top_roles.data = 0;
+                });
+        }
+
+        function _getNewRoles() {
+            roleFactory.getLatest(
+                $scope.new_roles.page,
+                $scope.results_per_page,
+                $scope.new_roles.sort_col,
+                $scope.new_roles.reverse
+                )
+                .success(function (data) {
+                    $scope.new_roles.data = data['results'];
+                    $scope.loading.newRoles = 0;
+                })
+                .error(function (error) {
+                    $scope.new_roles.data = [];
+                    $scope.loading.newRoles = 0;
+                });
+        }
+
+        function _getTopUsers() {
+            userFactory.getUsers(
+                $scope.top_users.page,
+                $scope.results_per_page,
+                $scope.top_users.sort_col,
+                $scope.top_users.reverse
+                )
+                .success(function (data) {
+                    data['results'].forEach(function(row) {
+                        row.num_roles = parseInt(row.num_roles);
+                        row.avg_role_score = parseInt(row.avg_role_score);
+                    });
+                    $scope.top_users.data = data['results'];
+                    $scope.loading.topUsers = 0;
+                })
+                .error(function (error) {
+                    $scope.top_users.data = [];
+                    $scope.loading.topUsers = 0;
+                });
+        }
+
+        function _getTopReviewers() {
+            userFactory.getUsers(
+                $scope.top_reviewers.page,
+                $scope.results_per_page,
+                $scope.top_reviewers.sort_col,
+                $scope.top_reviewers.reverse
+                )
+                .success(function (data) {
+                    data['results'].forEach(function(row) {
+                        row.num_ratings = parseInt(row.num_ratings);
+                    });
+                    $scope.top_reviewers.data = data['results'];
+                    $scope.loading.topReviewers = 0;
+                })
+                .error(function (error) {
+                    $scope.top_reviewers.data = [];
+                    $scope.loading.topReviewers = 0;
+                });
+        }
+
+        function _getNewUsers() {
+            userFactory.getUsers(
+                $scope.new_users.page,
+                $scope.results_per_page,
+                $scope.new_users.sort_col,
+                $scope.new_users.reverse
+                )
+                .success(function (data) {
+                    $scope.new_users.data = data['results'];
+                    $scope.loading.newUsers = 0;
+                })
+                .error(function (error) {
+                    $scope.new_users.data = [];
+                    $scope.loading.newUsers = 0;
+                });
+        }
     }
-    $scope.removeSaveState = $scope.$on('saveState', function(e, entry) {
-        if (typeof(Storage) !== "undefined") {
-            //localStorage.setItem(entry, JSON.stringify($scope[entry]));
-        }
-        });
 
-    $scope.toggle_item = function (item, sort_col) {
-        if (item.sort_col != sort_col) {
-            item.sort_col = sort_col;
-            item.data_function();
-        }
-        };
-
-    $scope.getCategories = function () {
-        categoryFactory.getCategories(
-            $scope.categories.sort_col,
-            $scope.categories.reverse
-            )
-            .success(function (data) {
-                $scope.categories.data = data.slice(0, $scope.results_per_page);
-                $scope.$emit('saveState', 'categories');
-                })
-            .error(function (error) {
-                });
-        };
-
-    $scope.getTopRoles = function () {
-        roleFactory.getLatest(
-            $scope.top_roles.page,
-            $scope.results_per_page,
-            $scope.top_roles.sort_col,
-            $scope.top_roles.reverse
-            )
-            .success(function (data) {
-                $scope.top_roles.data = data['results'];
-                $scope.$emit('saveState', 'top_roles');
-                })
-            .error(function (error) {
-                });
-        };
-
-    $scope.getNewRoles = function () {
-        roleFactory.getLatest(
-            $scope.new_roles.page,
-            $scope.results_per_page,
-            $scope.new_roles.sort_col,
-            $scope.new_roles.reverse
-            )
-            .success(function (data) {
-                $scope.new_roles.data = data['results'];
-                $scope.$emit('saveState', 'new_roles');
-                })
-            .error(function (error) {
-                });
-        };
-
-    $scope.getTopUsers = function () {
-        userFactory.getUsers(
-            $scope.top_users.page,
-            $scope.results_per_page,
-            $scope.top_users.sort_col,
-            $scope.top_users.reverse
-            )
-            .success(function (data) {
-                $scope.top_users.data = data['results'];
-                $scope.$emit('saveState', 'top_users');
-                })
-            .error(function (error) {
-                });
-        };
-
-    $scope.getTopReviewers = function () {
-        userFactory.getUsers(
-            $scope.top_reviewers.page,
-            $scope.results_per_page,
-            $scope.top_reviewers.sort_col,
-            $scope.top_reviewers.reverse
-            )
-            .success(function (data) {
-                $scope.top_reviewers.data = data['results'];
-                $scope.$emit('saveState', 'top_reviewers');
-                })
-            .error(function (error) {
-                });
-        };
-
-    $scope.getNewUsers = function () {
-        userFactory.getUsers(
-            $scope.new_users.page,
-            $scope.results_per_page,
-            $scope.new_users.sort_col,
-            $scope.new_users.reverse
-            )
-            .success(function (data) {
-                $scope.new_users.data = data['results'];
-                $scope.$emit('saveState', 'new_users');
-                })
-            .error(function (error) {
-                });
-    };
-
-    $scope.restore_state();
-    $scope.getCategories();
-    $scope.getTopRoles();
-    $scope.getNewRoles();
-    $scope.getTopUsers();
-    $scope.getTopReviewers();
-    $scope.getNewUsers();
-  }
-]);
+})(angular);
