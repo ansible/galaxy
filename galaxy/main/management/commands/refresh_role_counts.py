3eabeebf7f4606b5087ff9360b7248fbbe8ba3c0
Chris Houseknecht <chouseknecht@ansible.com>
2015-12-03 02:02:37 -0500

    Improve count status checking

--- galaxy/main/management/commands/refresh_role_counts.py
+++ galaxy/main/management/commands/refresh_role_counts.py
@@ -15,35 +15,37 @@ class Command(BaseCommand):
     help = 'Update each role with GitHub counts'
     
     def handle(self, *args, **options):
-
         agg = Role.objects.filter(is_valid=True,active=True).aggregate(Max('id'))
         max_id = agg['id__max']
         size = ceil(max_id / float(len(settings.GITHUB_TASK_USERS)))
-        objs = []
+        in_list = []
         print 'Refresh Role Counts'
-        for i in range(len(settings.GITHUB_TASK_USERS)):
-            start = size * i
-            end = size * (i + 1)
-            print 'User: %s' % settings.GITHUB_TASK_USERS[i]['username']
-            print 'Range: %d - %d' % (start, end)
-            r = RefreshRoleCount.objects.create(
-                state='PENDING',
-                description='User: %s Range: %s-%s' % (settings.GITHUB_TASK_USERS[i]['username'], start, end)
-            )
-            objs.append(r)
-            gh_api = Github(settings.GITHUB_TASK_USERS[i]['username'],settings.GITHUB_TASK_USERS[i]['password'])
-            refresh_role_counts.delay(start, end, gh_api, r)
-            print "Request submitted to Celery."
+        # for i in range(len(settings.GITHUB_TASK_USERS)):
+        i = 1
+        start = size * i
+        end = size * (i + 1)
+        print 'User: %s' % settings.GITHUB_TASK_USERS[i]['username']
+        print 'Range: %d - %d' % (start, end)
+        r = RefreshRoleCount.objects.create(
+            state='PENDING',
+            description='User: %s Range: %s-%s' % (settings.GITHUB_TASK_USERS[i]['username'], start, end)
+        )
+        in_list.append(r.id)
+        gh_api = Github(settings.GITHUB_TASK_USERS[i]['username'],settings.GITHUB_TASK_USERS[i]['password'])
+        refresh_role_counts.delay(start, end, gh_api, r)
+        print "Request submitted to Celery."
 
         finished = False
         started = time.time()
         while not finished:
             finished = True
-            for obj in objs:
+            for obj in RefreshRoleCount.objects.filter(pk__in=in_list,state__not='COMPLETED'):
                 if not obj.state == 'FINISHED':
                     finished = False
                 else:
                     print '%s Total: %s Passed: %s Failed: %s' % (obj.description, obj.failed + obj.passed, obj.passed, obj.failed)
+                    obj.state = 'COMPLETED'
+                    obj.save()
             time.sleep(60)
 
         elapsed = time.time() - started
