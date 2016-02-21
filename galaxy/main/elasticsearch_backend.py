3b404453e44ea9443e20f607b87ce4a3c2d64414
chouseknecht <chouseknecht@ansible.com>
2016-02-21 04:46:42 -0500

    Issue 126 - Make keyword search less fuzzy

---
+++ galaxy/main/elasticsearch_backend.py
@@ -0,0 +1,72 @@
+# (c) 2012-2016, Ansible, Inc. <support@ansible.com>
+#
+# This file is part of Ansible Galaxy
+#
+# Ansible Galaxy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible Galaxy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+from haystack.backends import BaseEngine
+from haystack.backends.elasticsearch_backend import ElasticsearchSearchBackend, ElasticsearchSearchQuery
+
+
+class GalaxyElasticSearchBackend(ElasticsearchSearchBackend):
+
+    # Settings to add an n-gram & edge n-gram analyzer.
+    DEFAULT_SETTINGS = {
+        'settings': {
+            "analysis": {
+                "analyzer": {
+                    "ngram_analyzer": {
+                        "type": "custom",
+                        "tokenizer": "standard",
+                        "filter": ["haystack_ngram", "lowercase"]
+                    },
+                    "edgengram_analyzer": {
+                        "type": "custom",
+                        "tokenizer": "standard",
+                        "filter": ["haystack_edgengram", "lowercase"]
+                    }
+                },
+                "tokenizer": {
+                    "haystack_ngram_tokenizer": {
+                        "type": "nGram",
+                        "min_gram": 3,
+                        "max_gram": 15,
+                    },
+                    "haystack_edgengram_tokenizer": {
+                        "type": "edgeNGram",
+                        "min_gram": 3,
+                        "max_gram": 15,
+                        "side": "front"
+                    }
+                },
+                "filter": {
+                    "haystack_ngram": {
+                        "type": "nGram",
+                        "min_gram": 3,
+                        "max_gram": 15
+                    },
+                    "haystack_edgengram": {
+                        "type": "edgeNGram",
+                        "min_gram": 3,
+                        "max_gram": 15
+                    }
+                }
+            }
+        }
+    }
+
+
+class ElasticsearchSearchEngine(BaseEngine):
+    backend = GalaxyElasticSearchBackend
+    query = ElasticsearchSearchQuery
