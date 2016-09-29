7c5a71a4dca8228d86e84147bcc4ec842a73259a
Chris Houseknecht <chouseknecht@ansible.com>
2016-09-28 22:39:02 -0400

    Use the same update-authors.py as AC

--- update-authors.py
+++ update-authors.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# cp this file to .git/hooks/pre-commit to automatically update AUTHORS.md with each commit
-
 from __future__ import absolute_import
 
 import logging
@@ -30,17 +28,14 @@ while log_entries:
             commit_data['deletions'] += int(count)
         else:
             commit_data['files'] += int(count)
-    if "Houseknecht" in author or "houseknecht" in author or "vagrant" in author:
-        author = "Chris Houskenecht <chousekn@redhat.com>"
     user_score = user_scores.setdefault(author, 0)
     user_scores[author] = user_score + commit_data['insertions'] + commit_data['deletions']
 
 user_scores = user_scores.items()
 sorted_user_scores = sorted(user_scores, key=lambda tpl: tpl[1], reverse=True)
 
-with open('AUTHORS','w') as f:
-    f.write(u"Ansible Container has been contribued to by the following authors:\n")
-    f.write(u"(This list is automatically generated - please file an issue for corrections)\n")
-    f.write(u"\n")
-    for author, _ in sorted_user_scores:
-        f.write(author + u"\n")
+print 'Ansible Container has been contribued to by the following authors:'
+print '(This list is automatically generated - please file an issue for corrections)'
+print ''
+for author, _ in sorted_user_scores:
+    print author
