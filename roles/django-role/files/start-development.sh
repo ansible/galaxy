27ff0a5d7c5e5fa4501b2e4ba90c89e4c19c95a9
Chris Houseknecht <chousekn@redhat.com>
2017-07-26 16:38:45 -0400

    Removes galaxy-manage install

--- roles/django-role/files/start-development.sh
+++ roles/django-role/files/start-development.sh
@@ -7,10 +7,19 @@ rm -f /galaxy_logs/*.log
 if [ ! -f /setup/dbinit.completed ]; then
      cd /setup
      /venv/bin/ansible-playbook -i inventory dbinit.yml
+     if [ "$?" != "0" ]; then 
+         exit 1
+     fi
      cd - 
 else
      /venv/bin/galaxy-manage makemigrations
+     if [ "$?" != "0" ]; then 
+         exit 1
+     fi
      /venv/bin/galaxy-manage migrate --noinput
+     if [ "$?" != "0" ]; then 
+         exit 1
+     fi
 fi
 
 /venv/bin/honcho start
