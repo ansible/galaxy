020a4beeefcb8869b54c7cb3a84009b085ff18d6
Oleksandr Saprykin <osapryki@redhat.com>
2017-10-04 15:51:09 +0200

    Fix django container failure on restart
    Use manage.py script from project directory for
    executing database migrations on container restart.

--- roles/django-role/files/start-development.sh
+++ roles/django-role/files/start-development.sh
@@ -2,22 +2,22 @@
 set +x
 
 # remove any lingering log files that might be owned by root
-rm -f /galaxy_logs/*.log 
+rm -f /galaxy_logs/*.log
 
 if [ ! -f /setup/dbinit.completed ]; then
      cd /setup
      /venv/bin/ansible-playbook -i inventory dbinit.yml
-     if [ "$?" != "0" ]; then 
+     if [ "$?" != "0" ]; then
          exit 1
      fi
-     cd - 
+     cd -
 else
-     /venv/bin/galaxy-manage makemigrations
-     if [ "$?" != "0" ]; then 
+     /venv/bin/python ./manage.py makemigrations
+     if [ "$?" != "0" ]; then
          exit 1
      fi
-     /venv/bin/galaxy-manage migrate --noinput
-     if [ "$?" != "0" ]; then 
+     /venv/bin/python ./manage.py migrate --noinput
+     if [ "$?" != "0" ]; then
          exit 1
      fi
 fi
