91481c092f32aa524781fb01f4a0029b474ebf35
Oleksandr Saprykin <osapryki@redhat.com>
2017-10-09 21:14:10 +0200

    Replace entrypoint from django image with command script

--- roles/django-role/files/start-development.sh
+++ roles/django-role/files/start-development.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
 set +x
 
 # remove any lingering log files that might be owned by root
 rm -f /galaxy_logs/*.log
 
-if [ -f /setup/dbinit.completed ]; then
+# TODO: Remove code duplicates
+if [ ! -f /setup/dbinit.completed ]; then
+     cd /setup
+     /venv/bin/ansible-playbook -i inventory dbinit.yml
+     if [ "$?" != "0" ]; then
+         exit 1
+     fi
+     cd -
+else
      /venv/bin/python ./manage.py makemigrations
      if [ "$?" != "0" ]; then
          exit 1
