67a58f06a540974ea2c9298d45d179a190f8e05b
Alexander Saprykin <osapryki@redhat.com>
2017-10-31 13:13:53 +0100

    Create build/static directory when building static

--- scripts/docker-release/build.sh
+++ scripts/docker-release/build.sh
@@ -5,6 +5,8 @@ set -o errexit
 
 GALAXY_VENV=${GALAXY_VENV:-/var/lib/galaxy/venv}
 
+mkdir -p build/static
+
 NODE_PATH=/tmp/node_modules/ /tmp/node_modules/.bin/gulp build
 
 ${GALAXY_VENV}/bin/python manage.py collectstatic --noinput --clear
