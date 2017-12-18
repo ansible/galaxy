488ba0f5938baf65c7d62728e7228128cfb9f381
Alexander Saprykin <osapryki@redhat.com>
2017-12-18 14:09:33 +0100

    Fix namespace refactor migration
    * Make model fields in migration in sync with actual models.
    * Refactor to fit 79 columns width according to PEP8.

--- galaxy/main/migrations/0065_namespace_refactor.py
+++ galaxy/main/migrations/0065_namespace_refactor.py
@@ -2,17 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import galaxy.main.mixins
 from django.conf import settings
+
+import galaxy.main.mixins
 import galaxy.main.fields
 
 
 COPY_NAMESPACE_DATA = """
 INSERT INTO main_providernamespace (
-description, created, modified, active, name, display_name, avatar_url, location,
-company, email, html_url, followers) 
-SELECT description, created, modified, active, a.namespace, name, avatar_url, location,
-company, email, html_url, followers
+  description, created, modified, active, name, display_name, 
+  avatar_url, location, company, email, html_url, followers) 
+SELECT 
+  description, created, modified, active, a.namespace, name, 
+  avatar_url, location, company, email, html_url, followers
 FROM main_namespace as a INNER JOIN
   (SELECT namespace, min(id) as id
    FROM main_namespace
@@ -20,30 +22,39 @@ FROM main_namespace as a INNER JOIN
 """
 
 ADD_REPO_GITHUB_USERS = """
-INSERT INTO main_providernamespace (created, modified, active, name, description)
+INSERT INTO main_providernamespace 
+  (created, modified, active, name, description)
 SELECT created, modified, true, b.github_user, b.github_user
 FROM main_repository as a INNER JOIN
   (SELECT github_user, min(id) as id
    FROM main_repository
-   GROUP BY github_user) as b ON a.github_user = b.github_user and a.id = b.id
-WHERE a.github_user not in (SELECT name FROM main_providernamespace WHERE name = a.github_user)   
+   GROUP BY github_user) as b 
+   ON a.github_user = b.github_user and a.id = b.id
+WHERE a.github_user not in (
+  SELECT name FROM main_providernamespace WHERE name = a.github_user)   
 """
 
 ADD_ROLE_NAMESPACE = """
-INSERT INTO main_providernamespace (created, modified, active, name, description)
-SELECT created, modified, true, a.namespace, a.namespace 
+INSERT INTO main_providernamespace 
+  (created, modified, active, name, description)
+SELECT 
+  created, modified, true, a.namespace, a.namespace 
 FROM main_content as a INNER JOIN
   (SELECT namespace, min(id) as id
    FROM main_content
    GROUP BY namespace) as b ON a.namespace = b.namespace and a.id = b.id
-WHERE a.namespace not in (SELECT name FROM main_providernamespace WHERE name = a.namespace)   
+WHERE a.namespace not in (
+  SELECT name FROM main_providernamespace WHERE name = a.namespace)   
 """
 
 NAMESPACE_FROM_PROVIDER_NAMESPACE = """
 INSERT INTO main_namespace (
-name, description, created, modified, active, original_name, avatar_url, location, company, email, html_url
+  name, description, created, modified, active, original_name, 
+  avatar_url, location, company, email, html_url
 )
-SELECT a.name, description, created, modified, true, a.name, avatar_url, location, company, email, html_url
+SELECT 
+  a.name, description, created, modified, true, a.name, 
+  avatar_url, location, company, email, html_url
 FROM main_providernamespace as a INNER JOIN
   (SELECT name, min(id) as id
    FROM main_providernamespace
@@ -74,9 +85,12 @@ WHERE u.github_user = n.name
 """
 
 ADD_GITHUB_PROVIDER = """
-INSERT INTO main_provider (created, modified, description, name, original_name, active)
-VALUES (CURRENT_DATE, CURRENT_DATE, 'Public GitHub', 'GitHub', 'GitHub', true)
+INSERT INTO main_provider 
+  (created, modified, description, name, original_name, active)
+VALUES 
+  (CURRENT_DATE, CURRENT_DATE, 'Public GitHub', 'GitHub', 'GitHub', true)
 """
+
 
 class Migration(migrations.Migration):
 
@@ -90,12 +104,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Provider',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(
+                    verbose_name='ID', serialize=False, auto_created=True,
+                    primary_key=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True)),
-                ('description', galaxy.main.fields.TruncatingCharField(default=b'', max_length=255, blank=True)),
+                ('description', galaxy.main.fields.TruncatingCharField(
+                     default=b'', max_length=255, blank=True)),
                 ('active', models.BooleanField(default=True, db_index=True)),
-                ('name', models.CharField(unique=True, max_length=512, db_index=True)),
+                ('name', models.CharField(
+                    unique=True, max_length=512, db_index=True)),
                 ('original_name', models.CharField(max_length=512)),
             ],
             options={
@@ -107,27 +125,49 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ProviderNamespace',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('description', galaxy.main.fields.TruncatingCharField(default=b'', max_length=255, blank=True)),
+                ('id', models.AutoField(
+                    verbose_name='ID', serialize=False, auto_created=True,
+                    primary_key=True)),
+                ('description', galaxy.main.fields.TruncatingCharField(
+                     default=b'', max_length=255, blank=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True)),
                 ('active', models.BooleanField(default=True, db_index=True)),
-                ('name', models.CharField(max_length=256, verbose_name=b'Name')),
-                ('display_name', models.CharField(max_length=256, null=True, verbose_name=b'Display Name', blank=True)),
-                ('avatar_url', models.CharField(max_length=256, null=True, verbose_name=b'Avatar URL', blank=True)),
-                ('location', models.CharField(max_length=256, null=True, verbose_name=b'Location', blank=True)),
-                ('company', models.CharField(max_length=256, null=True, verbose_name=b'Company', blank=True)),
-                ('email', models.CharField(max_length=256, null=True, verbose_name=b'Email', blank=True)),
-                ('html_url', models.CharField(max_length=256, null=True, verbose_name=b'Web Site', blank=True)),
-                ('followers', models.IntegerField(null=True, editable=False, verbose_name="Followers")),
-                ('namespace', models.ForeignKey(related_name='provider_namespaces', editable=False, to='main.Namespace',
-                                                null=True, verbose_name=b'Namespace')),
-                ('provider', models.ForeignKey(related_name='provider_namespaces', verbose_name=b'Provider',
-                                               to='main.Provider', null=True)),
+                ('name', models.CharField(
+                    max_length=256, verbose_name=b'Name')),
+                ('display_name', models.CharField(
+                    verbose_name=b'Display Name', max_length=256,
+                    null=True, editable=False, blank=True)),
+                ('avatar_url', models.CharField(
+                    verbose_name=b'Avatar URL', max_length=256,
+                    null=True, editable=False, blank=True)),
+                ('location', models.CharField(
+                    verbose_name=b'Location', max_length=256,
+                    null=True, editable=False, blank=True)),
+                ('company', models.CharField(
+                    verbose_name=b'Company Name', max_length=256,
+                    null=True, editable=False, blank=True)),
+                ('email', models.CharField(
+                    verbose_name=b'Email Address', max_length=256,
+                    null=True, editable=False, blank=True)),
+                ('html_url', models.CharField(
+                    verbose_name=b'Web Site URL', max_length=256,
+                    null=True, editable=False, blank=True)),
+                ('followers', models.IntegerField(
+                    null=True, editable=False,
+                    verbose_name="Followers")),
+                ('namespace', models.ForeignKey(
+                    related_name='namespaces', editable=False,
+                    to='main.Namespace', null=True,
+                    verbose_name=b'Namespace')),
+                ('provider', models.ForeignKey(
+                    related_name='provider', verbose_name=b'Provider',
+                    to='main.Provider', null=True)),
             ],
             options={
                 'ordering': ('provider', 'name',),
-                'unique_together': set([('provider', 'name'), ('namespace', 'provider', 'name')]),
+                'unique_together': {('provider', 'name'),
+                                    ('namespace', 'provider', 'name')},
             },
             bases=(models.Model, galaxy.main.mixins.DirtyMixin),
         ),
@@ -144,19 +184,34 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Namespace',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(
+                    verbose_name='ID', serialize=False,
+                    auto_created=True, primary_key=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True)),
-                ('description', galaxy.main.fields.TruncatingCharField(default=b'', max_length=255, blank=True)),
+                ('description', galaxy.main.fields.TruncatingCharField(
+                    default=b'', max_length=255, blank=True)),
                 ('active', models.BooleanField(default=True, db_index=True)),
-                ('name', models.CharField(unique=True, max_length=512, db_index=True)),
+                ('name', models.CharField(
+                    unique=True, max_length=512, db_index=True)),
                 ('original_name', models.CharField(max_length=512)),
-                ('avatar_url', models.CharField(max_length=256, null=True, verbose_name=b'Avatar URL', blank=True)),
-                ('location', models.CharField(max_length=256, null=True, verbose_name=b'Location', blank=True)),
-                ('company', models.CharField(max_length=256, null=True, verbose_name=b'Company Name', blank=True)),
-                ('email', models.CharField(max_length=256, null=True, verbose_name=b'Email Address', blank=True)),
-                ('html_url', models.CharField(max_length=256, null=True, verbose_name=b'Web Site URL', blank=True)),
-                ('owners', models.ManyToManyField(related_name='+', to=settings.AUTH_USER_MODEL)),
+                ('avatar_url', models.CharField(
+                    max_length=256, null=True,
+                    verbose_name=b'Avatar URL', blank=True)),
+                ('location', models.CharField(
+                    max_length=256, null=True,
+                    verbose_name=b'Location', blank=True)),
+                ('company', models.CharField(
+                    max_length=256, null=True,
+                    verbose_name=b'Company Name', blank=True)),
+                ('email', models.CharField(
+                    max_length=256, null=True,
+                    verbose_name=b'Email Address', blank=True)),
+                ('html_url', models.CharField(
+                    max_length=256, null=True,
+                    verbose_name=b'Web Site URL', blank=True)),
+                ('owners', models.ManyToManyField(
+                    related_name='+', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'ordering': ('name',),
