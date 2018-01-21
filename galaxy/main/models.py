a18436bc0261fc9ef93da20f08551f021543cef7
Chris Houseknecht <chousekn@redhat.com>
2018-01-20 19:02:59 -0500

    Fix lint issue

--- galaxy/main/models.py
+++ galaxy/main/models.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.forms.models import model_to_dict
-from django.utils.timezone import now
 
 from django.contrib.postgres.fields import ArrayField
 
