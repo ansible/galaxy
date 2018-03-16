52965fee80989be948625806b93dfe0d4762630e
Chris Houseknecht <chousekn@redhat.com>
2018-03-16 03:57:38 -0400

    Make static admin files available in dev

--- scripts/docker-dev/start-develop.sh
+++ scripts/docker-dev/start-develop.sh
@@ -6,7 +6,12 @@ set -o errexit
 cd /galaxy
 
 make waitenv
+make collectstatic
 make build/static
+
+if [ -d /galaxy/build/static/admin ]; then
+    cp -R /galaxy/build/static/admin /galaxy/galaxy/static/
+fi
 
 cd /galaxy/galaxyui
 yarn install
