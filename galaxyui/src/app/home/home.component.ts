8dca72afbd69f72977cff459a779cd6c24198077
Chris Houseknecht <chousekn@redhat.com>
2018-03-25 00:27:29 -0400

    Auto-resize cards

--- galaxyui/src/app/home/home.component.ts
+++ galaxyui/src/app/home/home.component.ts
@@ -1,20 +1,26 @@
 import {
 	Component,
-    OnInit
-}          from '@angular/core';
+    OnInit,
+    AfterViewInit
+} from '@angular/core';
 
 import {
 	CardConfig
 } from 'patternfly-ng/card/basic-card/card-config';
+
+import * as $ from 'jquery';
 
 @Component({
     selector:    'home',
     templateUrl: './home.component.html',
     styleUrls:   ['./home.component.less']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
 	downloadConfig: CardConfig;
+    shareConfig: CardConfig;
+    featureConfig: CardConfig;
+
 	headerTitle: string = "Home";
 
     constructor() {}
@@ -23,5 +29,43 @@ export class HomeComponent implements OnInit {
     	this.downloadConfig = {
 			titleBorder: true
     	} as CardConfig
+
+        this.shareConfig = {
+            titleBorder: true
+        } as CardConfig
+
+        this.featureConfig = {
+            titleBorder: true
+        } as CardConfig
+    }
+
+    onResize($event) {
+        this.setCardHeight();
+    }
+
+    ngAfterViewInit() {
+        this.setCardHeight();
+    }
+
+    // private 
+
+    private setCardHeight(): void {
+        // Set the cards to a consistent height
+        let windowHeight = window.innerHeight;
+        let windowWidth = window.innerWidth;
+        let height1 = $('#card-1').height();
+        let height2 = $('#card-2').height();
+        let height3 = $('#card-3').height();
+        let height = Math.max(height1, height2, height3);
+        console.log('Set height: ' + height);
+        if (windowWidth > 768 && !isNaN(height)) {
+            $('#card-1 .pfng-card').css('height', height);
+            $('#card-2 .pfng-card').css('height', height);
+            $('#card-3 .pfng-card').css('height', height);
+        } else {
+            $('#card-1 .pfng-card').css('height', 'auto');
+            $('#card-2 .pfng-card').css('height', 'auto');
+            $('#card-3 .pfng-card').css('height', 'auto');
+        }
     }
 }
