39b14dabf304ec565f3ba66bf381d5dbe6aa1e63
Alexander Saprykin <osapryki@redhat.com>
2018-04-12 12:41:09 +0200

    Fix get_git_version raising exception if not tag available

--- galaxy/common/version.py
+++ galaxy/common/version.py
@@ -42,15 +42,21 @@ def get_git_version():
     Returns package version in PEP440 format based on latest annotated
     version tag. Version tags should have prefix 'v'.
 
-    :return str: Package version string
-    :raises subprocess.CalledProcessError: If `git` command failed.
+    :return str: A version string.
+    :raises RuntimeError: If cannot determine git version string.
     """
-    tag_info = subprocess.check_output([
-        'git', 'describe', '--match', TAG_PREFIX + '*']
-    ).strip().lstrip(TAG_PREFIX)
-    chunks = tag_info.rsplit('-', 2)
+    try:
+        tag_info = subprocess.check_output([
+            'git', 'describe', '--always', '--match', TAG_PREFIX + '*']
+        ).strip()
+    except subprocess.CalledProcessError:
+        raise RuntimeError('Cannot determine git version string.')
 
-    if len(chunks) == 1:
-        return chunks[0]
+    if '-' in tag_info:
+        chunks = tag_info.lstrip(TAG_PREFIX).rsplit('-', 2)
+        return '{0}.dev{1}+{2}'.format(*chunks)
 
-    return '{0}.dev{1}+{2}'.format(*chunks)
+    if '.' in tag_info:
+        return tag_info.lstrip(TAG_PREFIX)
+
+    return '0.0.0.dev0+{0}'.format(tag_info)
