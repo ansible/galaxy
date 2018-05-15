4c06ff6aa35d50aeee6a510152a83a1ebcee397e
Alexander Saprykin <osapryki@redhat.com>
2018-05-15 15:42:21 +0200

    Fix flake8 issue

--- galaxy/importer/models.py
+++ galaxy/importer/models.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the Apache License
 # along with Galaxy.  If not, see <http://www.apache.org/licenses/>.
 
-import enum
 import collections
 
 import marshmallow as mm
