ac83bc39f4b64689841a09c5d80136e6f2ba7847
Alexander Saprykin <osapryki@redhat.com>
2018-05-17 11:15:09 +0200

    Fix functions declared in migration class scope

--- galaxy/main/migrations/0004_auto_20150824_1430.py
+++ galaxy/main/migrations/0004_auto_20150824_1430.py
@@ -5,19 +5,20 @@ from django.db import models, migrations
 
 import math
 
-class Migration(migrations.Migration):
-	
-    def round_score_up(apps, schema_editor):
-    	# Going forward each rating will contain a single score from 1 to 5.
-    	Ratings = apps.get_model("main", "RoleRating")
-    	for rating in Ratings.objects.all():
-        	rating.score = math.ceil(rating.score)
-        	rating.save()
 
+def round_score_up(apps, schema_editor):
+    # Going forward each rating will contain a single score from 1 to 5.
+    Ratings = apps.get_model("main", "RoleRating")
+    for rating in Ratings.objects.all():
+        rating.score = math.ceil(rating.score)
+        rating.save()
+
+
+class Migration(migrations.Migration):
     dependencies = [
         ('main', '0003_auto_20150824_1354'),
     ]
 
     operations = [
-    	migrations.RunPython(round_score_up),
+        migrations.RunPython(round_score_up),
     ]
--- galaxy/main/migrations/0018_auto_20151104_1701.py
+++ galaxy/main/migrations/0018_auto_20151104_1701.py
@@ -4,18 +4,21 @@ from __future__ import unicode_literals
 from django.db import models, migrations, transaction
 from django.db.models import Count
 
-class Migration(migrations.Migration):
 
-    @transaction.atomic
-    def set_namespace(apps, schema_editor):
-        Roles = apps.get_model("main", "Role")
-        for role in Roles.objects.all().order_by('github_user','name','-modified'):
-            try:
-                with transaction.atomic():
-                    role.namespace = role.owner.username
-                    role.save()
-            except:
-                pass
+@transaction.atomic
+def set_namespace(apps, schema_editor):
+    Roles = apps.get_model("main", "Role")
+    for role in Roles.objects.all().order_by('github_user', 'name',
+                                             '-modified'):
+        try:
+            with transaction.atomic():
+                role.namespace = role.owner.username
+                role.save()
+        except:
+            pass
+
+
+class Migration(migrations.Migration):
 
     dependencies = [
         ('main', '0017_auto_20151104_1700'),
