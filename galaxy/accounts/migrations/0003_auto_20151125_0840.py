cd84139c7e8fc70db9735763dcad9c31e68f23cc
Alexander Saprykin <osapryki@redhat.com>
2018-07-13 12:17:18 +0200

    Fix flake8 warning E501 in accounts migrations

--- galaxy/accounts/migrations/0003_auto_20151125_0840.py
+++ galaxy/accounts/migrations/0003_auto_20151125_0840.py
@@ -6,19 +6,21 @@ from django.db import models, migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('accounts', '0002_auto_20150803_1328'),
-    ]
+    dependencies = [('accounts', '0002_auto_20150803_1328')]
 
     operations = [
         migrations.AddField(
             model_name='customuser',
             name='github_avatar',
-            field=models.CharField(max_length=254, verbose_name='github avatar', blank=True),
+            field=models.CharField(
+                max_length=254, verbose_name='github avatar', blank=True
+            ),
         ),
         migrations.AddField(
             model_name='customuser',
             name='github_user',
-            field=models.CharField(max_length=254, verbose_name='github user', blank=True),
+            field=models.CharField(
+                max_length=254, verbose_name='github user', blank=True
+            ),
         ),
     ]
