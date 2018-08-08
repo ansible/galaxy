20a054d05e6998bb74e8473d1f139c3b4a7e6e9b
David Newswanger <dnewswan@redhat.com>
2018-08-08 16:05:53 -0400

    Fix patternfly error messages.

--- galaxyui/src/app/app.module.ts
+++ galaxyui/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 
-import { ToastNotificationModule, VerticalNavigationModule } from 'patternfly-ng';
+import { AboutModalModule, ToastNotificationListModule, VerticalNavigationModule } from 'patternfly-ng';
 
 import { BsDropdownModule, ModalModule, TooltipModule } from 'ngx-bootstrap';
 
@@ -55,8 +55,7 @@ import { VendorsModule } from './vendors/vendors.module';
         HomeModule,
         LoginModule,
         MyContentModule,
-        ModalModule,
-        ToastNotificationModule,
+        ToastNotificationListModule,
         ContentDetailModule,
         ExceptionPagesModule,
         VendorsModule,
