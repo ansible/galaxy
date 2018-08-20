c59832780424495597f889eeb378125c77e7b9da
Alexander Saprykin <osapryki@redhat.com>
2018-08-20 11:56:39 +0200

    Disable mail admins logging handler
    Galaxy is not configured to send emails at the moment.

--- galaxy/settings/default.py
+++ galaxy/settings/default.py
@@ -322,11 +322,6 @@ LOGGING = {
     },
 
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
@@ -340,11 +335,6 @@ LOGGING = {
     },
 
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
         'django': {
             'handlers': ['console'],
             'level': 'DEBUG',
