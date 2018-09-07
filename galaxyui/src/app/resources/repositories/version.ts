d9be9f1cbbdef150f770456ac2176ecb68133873
Chris Houseknecht <chousekn@redhat.com>
2018-09-07 16:05:48 -0400

    Show versions at the repo level

---
+++ galaxyui/src/app/resources/repositories/version.ts
@@ -0,0 +1,14 @@
+export class Version {
+    id: number;
+    version: string;
+    tag: string;
+    commit_date: string;
+    commit_sha: string;
+    download_url: string;
+    url: string;
+    related: string;
+    summary_fields: string;
+    created: string;
+    modified: string;
+    active: boolean;
+}
