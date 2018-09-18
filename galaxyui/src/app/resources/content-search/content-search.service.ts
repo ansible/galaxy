f3b1c0706453afc11a6d2f5006d6bdf1373755c7
David Newswanger <dnewswan@redhat.com>
2018-09-18 14:59:27 -0400

    Set search service to use object for params

--- galaxyui/src/app/resources/content-search/content-search.service.ts
+++ galaxyui/src/app/resources/content-search/content-search.service.ts
@@ -12,14 +12,10 @@ import { ContentResponse } from './content';
 export class ContentSearchService {
     constructor(private http: HttpClient, private notificationService: NotificationService) {}
 
-    private url = '/api/v1/search/content/';
+    private url = '/api/v1/search/content';
 
-    query(query?: string): Observable<ContentResponse> {
-        let requestUrl = this.url;
-        if (query) {
-            requestUrl += `?${query}`;
-        }
-        return this.http.get<ContentResponse>(requestUrl).pipe(
+    query(params?: any): Observable<ContentResponse> {
+        return this.http.get<ContentResponse>(this.url, { params: params }).pipe(
             tap(_ => this.log('fetched content')),
             catchError(this.handleError('Query', {} as ContentResponse)),
         );
