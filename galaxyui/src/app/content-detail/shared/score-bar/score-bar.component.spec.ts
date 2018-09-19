70986b697111f2a1884512f60a26bd42ec7492e2
David Newswanger <dnewswan@redhat.com>
2018-09-19 10:11:20 -0400

    prettier

--- galaxyui/src/app/content-detail/shared/score-bar/score-bar.component.spec.ts
+++ galaxyui/src/app/content-detail/shared/score-bar/score-bar.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ScoreBarComponent } from './score-bar.component';
 
 describe('ScoreBarComponent', () => {
-  let component: ScoreBarComponent;
-  let fixture: ComponentFixture<ScoreBarComponent>;
+    let component: ScoreBarComponent;
+    let fixture: ComponentFixture<ScoreBarComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ScoreBarComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ScoreBarComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ScoreBarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ScoreBarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
