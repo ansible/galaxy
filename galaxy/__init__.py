b0ac4e8d38e27bc5af47e4e902d710ab9d12b587
Alexander Saprykin <cutwatercore@gmail.com>
2018-10-23 11:36:28 +0200

    Refactoring: Remove unused and legacy code (#1205)
    * Remove unused function
    
    * Remove legacy galaxy-manage version wrapper
    
    * Remove suppression of deprecation warnings
    
    Since Django 1.11 deprecation warnings are no longer loud
    by default [1].
    
    [1] https://docs.djangoproject.com/en/1.11/releases/1.11/#deprecating-warnings-are-no-longer-loud-by-default
    
    * Remove unused `settings.version`
    
    * Refactor wsgi and manage entry points
    
    * Remove setting up mimetypes for wsgi entry point.
    * Remove prepare_env function as it duplicates now wsgi code.

--- galaxy/__init__.py
+++ galaxy/__init__.py
@@ -17,7 +17,6 @@
 
 import os
 import sys
-import warnings
 import mimetypes
 
 from galaxy.common import version
@@ -26,44 +25,12 @@ __all__ = ['__version__']
 __version__ = version.get_package_version(__name__)
 
 
-def find_commands(management_dir):
-    # Modified version of function from django/core/management/__init__.py.
-    command_dir = os.path.join(management_dir, 'commands')
-    commands = []
-    try:
-        for f in os.listdir(command_dir):
-            if f.startswith('_'):
-                continue
-            elif f.endswith('.py') and f[:-3] not in commands:
-                commands.append(f[:-3])
-            elif f.endswith('.pyc') and f[:-4] not in commands:
-                commands.append(f[:-4])
-    except OSError:
-        pass
-    return commands
-
-
-def prepare_env():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'galaxy.settings.default')
-    from django.conf import settings
-    if not settings.DEBUG:
-        warnings.simplefilter('ignore', DeprecationWarning)
-    # import django.utils
-    settings.version = __version__
-    _fix_mimetypes()
-
-
 def manage():
-    # Prepare the galaxy environment.
-    prepare_env()
-    # Now run the command (or display the version).
-    from django.core.management import execute_from_command_line
-    if len(sys.argv) >= 2 and sys.argv[1] in ('version', '--version'):
-        sys.stdout.write('galaxy-%s\n' % __version__)
-    else:
-        execute_from_command_line(sys.argv)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'galaxy.settings.default')
 
-
-def _fix_mimetypes():
+    # Fix for serving by dev server SVG images with correct content type
     mimetypes.add_type("image/svg+xml", ".svg", True)
     mimetypes.add_type("image/svg+xml", ".svgz", True)
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
