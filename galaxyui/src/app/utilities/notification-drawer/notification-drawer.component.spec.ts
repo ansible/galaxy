313fe0b2bdf2df0025fa3437bb06157705bcbffc
David Newswanger <dnewswan@redhat.com>
2018-10-25 14:10:12 -0400

    prettier

--- galaxyui/src/app/utilities/notification-drawer/notification-drawer.component.spec.ts
+++ galaxyui/src/app/utilities/notification-drawer/notification-drawer.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotificationDrawerComponent } from './notification-drawer.component';
 
 describe('NotificationDrawerComponent', () => {
-  let component: NotificationDrawerComponent;
-  let fixture: ComponentFixture<NotificationDrawerComponent>;
+    let component: NotificationDrawerComponent;
+    let fixture: ComponentFixture<NotificationDrawerComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NotificationDrawerComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [NotificationDrawerComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NotificationDrawerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NotificationDrawerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
