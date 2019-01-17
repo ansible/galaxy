a9252501e63135ba121e430f775c3c4f5ff4c748
David Newswanger <dnewswan@redhat.com>
2019-01-17 15:42:36 -0500

    Migrate page header CSS over to react.

--- galaxyui/src/app/authors-react/react-components/page-header.tsx
+++ galaxyui/src/app/authors-react/react-components/page-header.tsx
@@ -35,30 +35,25 @@ export class PageHeader extends React.Component<IPageHeaderProp, {}> {
                 const name = title_list[idx - 1];
                 const path = item;
                 breadcrumbs.push(
-                    <li>
+                    <li className='hide-on-mobile'>
+                        <Link to={path}>{name}</Link>
                         <span className='separator'>
                             <span className='fa fa-angle-right' />
                         </span>
-                        <Link to={path}>{name}</Link>
                     </li>,
                 );
             }
             if (idx === title_list.length - 1) {
                 const name = item;
-                breadcrumbs.push(
-                    <li className='hide-on-mobile'>
-                        <span className='separator'>
-                            <span className='fa fa-angle-right' />
-                        </span>
-                        {name}
-                    </li>,
-                );
+                breadcrumbs.push(<li>{name}</li>);
             }
         });
 
         return (
-            <div className='page-header'>
-                <ul className='page-header-list'>{breadcrumbs}</ul>
+            <div className='page-header-react-wrapper'>
+                <div className='page-header'>
+                    <ul className='page-header-list'>{breadcrumbs}</ul>
+                </div>
             </div>
         );
     }
