ce6cf27c2f9691cbbf9b3e662ae74290f23f05f0
David Newswanger <gamma.dave@gmail.com>
2019-02-13 12:54:35 -0500

    Send user back to first page when they make searches. (#1550)

--- galaxyui/src/app/search/search.component.ts
+++ galaxyui/src/app/search/search.component.ts
@@ -269,7 +269,7 @@ export class SearchComponent implements OnInit, AfterViewInit {
         }
         sortParams += $event.field.id;
         this.queryParams['order_by'] = sortParams;
-        this.searchContent();
+        this.searchEntered();
     }
 
     filterChanged($event: FilterEvent): void {
@@ -325,7 +325,7 @@ export class SearchComponent implements OnInit, AfterViewInit {
             this.appliedFilters = [];
             this.contentItems = [];
         }
-        this.searchContent();
+        this.searchEntered();
     }
 
     getToolbarConfig(): ToolbarConfig {
@@ -372,8 +372,15 @@ export class SearchComponent implements OnInit, AfterViewInit {
 
         if (update) {
             this.setAppliedFilters(this.queryParams);
-            this.searchContent();
+            this.searchEntered();
         }
+    }
+
+    searchEntered() {
+        this.queryParams['page'] = 1;
+        this.pageNumber = 1;
+        this.paginationConfig.pageNumber = 1;
+        this.searchContent();
     }
 
     searchContent() {
