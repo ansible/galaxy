a27976c8255deb8a905452edc5d68d4f2da2b797
David Newswanger <dnewswan@redhat.com>
2019-02-27 11:49:30 -0500

    Added tooltips to explain diferent import systems.

--- galaxyui/src/app/react/components/import-modal/pick-import-type.tsx
+++ galaxyui/src/app/react/components/import-modal/pick-import-type.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button } from 'patternfly-react';
+import { Button, OverlayTrigger, Tooltip } from 'patternfly-react';
 import { View } from '../../shared-types/add-repository';
 
 interface IProps {
@@ -11,23 +11,48 @@ export class PickImportType extends React.Component<IProps, {}> {
         return (
             <div className='add-content-type-selector'>
                 <div className='button-container'>
-                    <Button
-                        bsSize='large'
-                        onClick={() => this.props.setView(View.RepoImport)}
-                    >
-                        <i className='fa fa-github' /> Import from GitHub
-                    </Button>
-
-                    <Button
-                        className='right-button'
-                        bsSize='large'
-                        onClick={() =>
-                            this.props.setView(View.CollectionImport)
+                    <OverlayTrigger
+                        overlay={
+                            <Tooltip>
+                                Legacy Role import. Does not support Collection
+                                format.
+                            </Tooltip>
                         }
+                        placement='top'
+                        trigger={['hover', 'focus']}
+                        rootClose={false}
                     >
-                        <i className='pficon-folder-open' /> Upload New
-                        Collection
-                    </Button>
+                        <Button
+                            bsSize='large'
+                            onClick={() => this.props.setView(View.RepoImport)}
+                        >
+                            <i className='fa fa-github' /> Import Role from
+                            GitHub
+                        </Button>
+                    </OverlayTrigger>
+
+                    <OverlayTrigger
+                        overlay={
+                            <Tooltip>
+                                Used for distributing Galaxy hosted roles,
+                                modules and plugins.
+                            </Tooltip>
+                        }
+                        placement='top'
+                        trigger={['hover', 'focus']}
+                        rootClose={false}
+                    >
+                        <Button
+                            className='right-button'
+                            bsSize='large'
+                            onClick={() =>
+                                this.props.setView(View.CollectionImport)
+                            }
+                        >
+                            <i className='pficon-folder-open' /> Upload New
+                            Collection
+                        </Button>
+                    </OverlayTrigger>
                 </div>
             </div>
         );
