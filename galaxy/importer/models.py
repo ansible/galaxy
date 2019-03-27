15d1fab1b38aac03069a601b78ef5c6081087e03
Andrew Crosby <acrosby@redhat.com>
2019-03-27 14:33:07 -0400

    Update docstrings and formatting
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- galaxy/importer/models.py
+++ galaxy/importer/models.py
@@ -88,7 +88,10 @@ class Repository(object):
 
 @attr.s(frozen=True)
 class BaseCollectionInfo(object):
-    """Represents collection_info metadata in collection manifest."""
+    '''Represents collection_info metadata in collection manifest.
+
+    Includes data validation expected when collection is built.
+    '''
 
     namespace = attr.ib(default=None)
     name = attr.ib(default=None)
@@ -126,11 +129,13 @@ class BaseCollectionInfo(object):
     @name.validator
     @version.validator
     def _check_required(self, attribute, value):
+        '''Check that value is present'''
         if not value:
             self.value_error("'%s' is required" % attribute.name)
 
     @version.validator
     def _check_version_format(self, attribute, value):
+        '''Check that version is in semantic version format'''
         if not semantic_version.validate(value):
             self.value_error("Expecting 'version' to be in semantic version "
                              "format, instead found '%s'." % value)
@@ -139,6 +144,7 @@ class BaseCollectionInfo(object):
     @tags.validator
     @license.validator
     def _check_list_of_str(self, attribute, value):
+        '''Check that value is a list of strings'''
         err_msg = "Expecting '%s' to be a list of strings"
         if not isinstance(value, list):
             self.value_error(err_msg % attribute.name)
@@ -148,6 +154,7 @@ class BaseCollectionInfo(object):
 
     @license.validator
     def _check_licenses(self, attribute, value):
+        '''Check that all licenses in license list are valid'''
         # load or return already loaded data
         valid_license_ids = spdx_licenses.get_spdx()
 
@@ -164,6 +171,7 @@ class BaseCollectionInfo(object):
 
     @staticmethod
     def _is_valid_license_id(license_id, valid_license_ids):
+        '''Check if license_id is valid and non-deprecated SPDX ID'''
         if license_id is None:
             return False
 
@@ -179,6 +187,7 @@ class BaseCollectionInfo(object):
 
     @dependencies.validator
     def _check_dependencies_type(self, attribute, value):
+        '''Check type of dependencies dictionary and its contents'''
         if not isinstance(value, dict) or value is None:
             self.value_error("Expecting '%s' to be a dict" % attribute.name)
         for k, v in value.items():
@@ -187,6 +196,7 @@ class BaseCollectionInfo(object):
 
     @tags.validator
     def _check_tags(self, attribute, value):
+        '''Check value against tag regular expression'''
         for tag in value:
             # TODO update tag format once resolved
             # https://github.com/ansible/galaxy/issues/1563
@@ -196,6 +206,7 @@ class BaseCollectionInfo(object):
     @namespace.validator
     @name.validator
     def _check_name(self, attribute, value):
+        '''Check value against name regular expression'''
         if not re.match(constants.NAME_REGEXP, value):
             self.value_error("'%s' has invalid format: %s" %
                              (attribute.name, value))
@@ -216,21 +227,27 @@ class BaseCollectionInfo(object):
 
 @attr.s(frozen=True)
 class GalaxyCollectionInfo(BaseCollectionInfo):
-    """Represents collection_info metadata in galaxy."""
+    '''Represents collection_info metadata in galaxy.
+
+    Includes additional data validation that is specific to galaxy.
+    '''
 
     def get_json(self):
         return self.__dict__
 
     def _check_required(self, name):
+        '''Check that value is present'''
         if not getattr(self, name):
             self.value_error("'%s' is required by galaxy" % name)
 
     def _check_non_null_str(self, name):
+        '''Check that if value is present, it must be a string'''
         value = getattr(self, name)
         if value is not None and not isinstance(value, str):
             self.value_error("'%s' must be a string" % name)
 
     def _check_dependencies(self):
+        '''Check dependencies and matching version present in database'''
         for dep_col, ver_spec in self.dependencies.items():
             ns_name, name = dep_col.split('.')
 
@@ -270,19 +287,18 @@ class GalaxyCollectionInfo(BaseCollectionInfo):
                              'version found: %s %s' % (dep_col, ver_spec))
 
     def __attrs_post_init__(self):
+        '''Additional galaxy checks called post init'''
         super().__attrs_post_init__()
-        non_null_str_fields = [
-            'description',
-            'repository',
-            'documentation',
-            'homepage',
-            'issues',
-        ]
-
         self._check_required('readme')
         self._check_required('authors')
         self._check_dependencies()
-        for field in non_null_str_fields:
+        for field in [
+                        'description',
+                        'repository',
+                        'documentation',
+                        'homepage',
+                        'issues',
+                     ]:
             self._check_non_null_str(field)
 
 
