79e4224ac6964587671ef73ae260f6349fa478ea
Andrew Crosby <acrosby@redhat.com>
2019-03-27 17:51:04 -0400

    Support license as list and license_file in collection manifest (#1692)
    * Expect license as list and validate each list item
    * Allow license_file as optional str
    * Make required to have either license or license_file
    * Update docstrings and formatting
    * Update unit tests

--- galaxy/importer/models.py
+++ galaxy/importer/models.py
@@ -88,15 +88,16 @@ class Repository(object):
 
 @attr.s(frozen=True)
 class BaseCollectionInfo(object):
-    """Represents collection_info metadata in collection manifest."""
+    '''Represents collection_info metadata in collection manifest.
+
+    Includes data validation expected when collection is built.
+    '''
 
     namespace = attr.ib(default=None)
     name = attr.ib(default=None)
     version = attr.ib(default=None)
-    license = attr.ib(default=None)
+    license = attr.ib(factory=list)
     description = attr.ib(default=None)
-
-    # TODO: see how to handle license_list
 
     repository = attr.ib(default=None)
     documentation = attr.ib(default=None)
@@ -105,9 +106,13 @@ class BaseCollectionInfo(object):
 
     authors = attr.ib(factory=list)
     tags = attr.ib(factory=list)
-    readme = attr.ib(default=None,
-                     validator=attr.validators.optional(
-                        attr.validators.instance_of(str)))
+
+    license_file = attr.ib(
+        default=None,
+        validator=attr.validators.optional(attr.validators.instance_of(str)))
+    readme = attr.ib(
+        default=None,
+        validator=attr.validators.optional(attr.validators.instance_of(str)))
 
     dependencies = attr.ib(factory=dict,
                            converter=convert_none_to_empty_dict)
@@ -123,36 +128,23 @@ class BaseCollectionInfo(object):
     @namespace.validator
     @name.validator
     @version.validator
-    @license.validator
     def _check_required(self, attribute, value):
+        '''Check that value is present'''
         if not value:
             self.value_error("'%s' is required" % attribute.name)
 
     @version.validator
     def _check_version_format(self, attribute, value):
+        '''Check that version is in semantic version format'''
         if not semantic_version.validate(value):
             self.value_error("Expecting 'version' to be in semantic version "
                              "format, instead found '%s'." % value)
 
-    @license.validator
-    def _check_license(self, attribute, value):
-        # load or return already loaded data
-        licenses = spdx_licenses.get_spdx()
-
-        valid = licenses.get(value, None)
-        if valid is None:
-            self.value_error("Expecting 'license' to be a valid SPDX license "
-                             "ID, instead found '%s'. "
-                             "For more info, visit https://spdx.org" % value)
-
-        # license was in list, but is deprecated
-        if valid and valid.get('deprecated', None):
-            self.value_error("Expecting 'license' SPDX ID to not be "
-                             "deprecated: %s" % value)
-
     @authors.validator
     @tags.validator
+    @license.validator
     def _check_list_of_str(self, attribute, value):
+        '''Check that value is a list of strings'''
         err_msg = "Expecting '%s' to be a list of strings"
         if not isinstance(value, list):
             self.value_error(err_msg % attribute.name)
@@ -160,8 +152,42 @@ class BaseCollectionInfo(object):
             if not isinstance(list_item, str):
                 self.value_error(err_msg % attribute.name)
 
+    @license.validator
+    def _check_licenses(self, attribute, value):
+        '''Check that all licenses in license list are valid'''
+        # load or return already loaded data
+        valid_license_ids = spdx_licenses.get_spdx()
+
+        invalid_licenses = [license_id for license_id in value
+                            if not self._is_valid_license_id(
+                                license_id, valid_license_ids)]
+        if invalid_licenses:
+            self.value_error(
+                "Expecting 'license' to be a list of valid SPDX license "
+                "identifiers, instead found invalid license identifiers: '%s' "
+                "in 'license' value %s. "
+                "For more info, visit https://spdx.org" %
+                (','.join(invalid_licenses), value))
+
+    @staticmethod
+    def _is_valid_license_id(license_id, valid_license_ids):
+        '''Check if license_id is valid and non-deprecated SPDX ID'''
+        if license_id is None:
+            return False
+
+        valid = valid_license_ids.get(license_id, None)
+        if valid is None:
+            return False
+
+        # license was in list, but is deprecated
+        if valid and valid.get('deprecated', None):
+            return False
+
+        return True
+
     @dependencies.validator
     def _check_dependencies_type(self, attribute, value):
+        '''Check type of dependencies dictionary and its contents'''
         if not isinstance(value, dict) or value is None:
             self.value_error("Expecting '%s' to be a dict" % attribute.name)
         for k, v in value.items():
@@ -170,6 +196,7 @@ class BaseCollectionInfo(object):
 
     @tags.validator
     def _check_tags(self, attribute, value):
+        '''Check value against tag regular expression'''
         for tag in value:
             # TODO update tag format once resolved
             # https://github.com/ansible/galaxy/issues/1563
@@ -179,28 +206,48 @@ class BaseCollectionInfo(object):
     @namespace.validator
     @name.validator
     def _check_name(self, attribute, value):
+        '''Check value against name regular expression'''
         if not re.match(constants.NAME_REGEXP, value):
             self.value_error("'%s' has invalid format: %s" %
                              (attribute.name, value))
 
+    def __attrs_post_init__(self):
+        '''Checks called post init validation'''
+        self._check_license_or_license_file(self.license, self.license_file)
+
+    def _check_license_or_license_file(self, license_ids, license_file):
+        '''Confirm presence of either license or license_file'''
+        if license_ids or license_file:
+            return
+        self.value_error(
+            "Valid values for 'license' or 'license_file' are required. "
+            "But 'license' (%s) and 'license_file' (%s) were invalid." %
+            (license_ids, license_file))
+
 
 @attr.s(frozen=True)
 class GalaxyCollectionInfo(BaseCollectionInfo):
-    """Represents collection_info metadata in galaxy."""
+    '''Represents collection_info metadata in galaxy.
+
+    Includes additional data validation that is specific to galaxy.
+    '''
 
     def get_json(self):
         return self.__dict__
 
     def _check_required(self, name):
+        '''Check that value is present'''
         if not getattr(self, name):
             self.value_error("'%s' is required by galaxy" % name)
 
     def _check_non_null_str(self, name):
+        '''Check that if value is present, it must be a string'''
         value = getattr(self, name)
         if value is not None and not isinstance(value, str):
             self.value_error("'%s' must be a string" % name)
 
     def _check_dependencies(self):
+        '''Check dependencies and matching version present in database'''
         for dep_col, ver_spec in self.dependencies.items():
             ns_name, name = dep_col.split('.')
 
@@ -240,18 +287,18 @@ class GalaxyCollectionInfo(BaseCollectionInfo):
                              'version found: %s %s' % (dep_col, ver_spec))
 
     def __attrs_post_init__(self):
-        non_null_str_fields = [
-            'description',
-            'repository',
-            'documentation',
-            'homepage',
-            'issues',
-        ]
-
+        '''Additional galaxy checks called post init'''
+        super().__attrs_post_init__()
         self._check_required('readme')
         self._check_required('authors')
         self._check_dependencies()
-        for field in non_null_str_fields:
+        for field in [
+                        'description',
+                        'repository',
+                        'documentation',
+                        'homepage',
+                        'issues',
+                     ]:
             self._check_non_null_str(field)
 
 
