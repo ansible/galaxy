03a05375cd03e0c6743cb36582d37245f2472ad4
Andrew Crosby <acrosby@redhat.com>
2019-03-28 14:15:36 -0400

    Fix latest migration name and dependency (#1697)
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- galaxy/main/migrations/0134_update_contenttype.py
+++ galaxy/main/migrations/0134_update_contenttype.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', 'update_collecion_scores'),
+        ('main', '0133_collection_tasks'),
     ]
 
     operations = [
