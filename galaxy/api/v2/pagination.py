03b19bcd9464d92043eeeec22555ceb031c40eaa
Andrew Crosby <acrosby@redhat.com>
2019-04-18 15:55:43 -0400

    Have custom pagination use absolute url and page_size of 10
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- galaxy/api/v2/pagination.py
+++ galaxy/api/v2/pagination.py
@@ -16,39 +16,9 @@
 # along with Galaxy.  If not, see <http://www.apache.org/licenses/>.
 
 from rest_framework import pagination
-from rest_framework.utils.urls import remove_query_param, replace_query_param
 
 
 class CustomPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
-
-    # overrides to create relative URL
-    def get_next_link(self):
-        if not self.page.has_next():
-            return None
-        url = self.request.get_full_path()
-        page_number = self.page.next_page_number()
-        return replace_query_param(url, self.page_query_param, page_number)
-
-    # overrides to create relative URL
-    def get_previous_link(self):
-        if not self.page.has_previous():
-            return None
-        url = self.request.get_full_path()
-        page_number = self.page.previous_page_number()
-        if page_number == 1:
-            return remove_query_param(url, self.page_query_param)
-        return replace_query_param(url, self.page_query_param, page_number)
-
-    # TODO: decide - keep if want to rename "next" to "next_link", etc.
-    # def get_paginated_response(self, data):
-    #     next_link = self.get_next_link()
-    #     previous_link = self.get_previous_link()
-    #     return response.Response(OrderedDict([
-    #         ('count', self.page.paginator.count),
-    #         ('next_link', next_link),
-    #         ('previous_link', previous_link),
-    #         ('results', data),
-    #     ]))
