2cf106ffc8ee92fc0410409b991a1b9d261268d6
Jason Yundt <jason@jasonyundt.email>
2023-02-18 07:31:27 -0500

    Simplify update-authors.py
    Before this change, there was effectively two for loops, one for the
    list comprehension and one to iterate over the resulting list.
    Generating the list wasn’t necessary since the code would just
    immediately iterate over the list after it was generated, and then never
    do anything with the list ever again.

--- update-authors.py
+++ update-authors.py
@@ -7,10 +7,9 @@ author_re = re.compile(r'(?<=\t).*')
 
 git_log = subprocess.check_output(['git', 'shortlog', '--summary', '--email'])
 log_entries = git_log.decode('utf-8').strip().split('\n')
-authors = [author_re.search(entry).group(0) for entry in log_entries]
 
 
 print("Galaxy has been contribued to by the following authors:\n"
       "This list is automatically generated - please file an issue for corrections)\n")
-for author in authors:
-    print(author)
+for entry in log_entries:
+    print(author_re.search(entry).group(0))
